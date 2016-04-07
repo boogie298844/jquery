ed5ff1abd06bb34db0c29851de714a3522e57fd7
boogie22222 <sbudrow1@x9da13.harford.edu>
2016-04-06 21:10:47 -0400

    created more stuff using query

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 $(document).ready( function () {
-   alert("still working");
+
+   $("#meow").click( function() {
+     $("#animals").append("<img class='animal' src='images/nyancat.png' alt='nyancat' />");
+
+     $("#bulbasaur").click( function() {
+       $("#leaf").append("<img class='leaf' src='images/bulbasaur.png' alt='pokemon' />");
+
+   });
+
 });
